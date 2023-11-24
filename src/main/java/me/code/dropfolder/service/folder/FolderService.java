f95917cb6fdfc2ab0d25bbed109d2a4d1d107488
jm <johan.moller040@gmail.com>
2023-11-24 16:57:18 +0100

    refactoring; refactored folderservice, loginvalidator and uniquenamegeneratorutil, added comments for all

--- src/main/java/me/code/dropfolder/service/folder/FolderService.java
+++ src/main/java/me/code/dropfolder/service/folder/FolderService.java
@@ -12,12 +12,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for managing folder-related operations, such as creating folders.
+ * <p>
+ * This class handles the creation of folders for a specified user, ensuring unique folder names.
+ */
 @Service
 public class FolderService {
     private final FolderRepository folderRepository;
     private final JpQueryUtil query;
     private final UniqueNameGeneratorUtil nameGenerator;
 
+    /**
+     * Constructs a new FolderService with the given dependencies.
+     *
+     * @param folderRepository The repository for folder-related database operations.
+     * @param query            The utility for executing JPQL queries related to folders and users.
+     * @param nameGenerator    The utility for generating unique names for folders and files.
+     */
     @Autowired
     public FolderService(FolderRepository folderRepository, JpQueryUtil query, UniqueNameGeneratorUtil nameGenerator) {
         this.folderRepository = folderRepository;
@@ -25,15 +37,24 @@ public class FolderService {
         this.nameGenerator = nameGenerator;
     }
 
+    /**
+     * Creates a new folder for the specified user with the given name.
+     *
+     * @param userId The ID of the user initiating the folder creation.
+     * @param name   The name of the new folder.
+     * @return A SuccessDto indicating the result of the folder creation operation.
+     * @throws FolderCreationFailureException If the folder creation operation fails.
+     */
     public SuccessDto createFolder(long userId, String name) {
         try {
             User user = query.loadUserById(userId);
-            String uniqueName = nameGenerator.getUniqueFolderName(user, name);
+            Folder newFolder = new Folder(name, user);
+            nameGenerator.setUniqueFolderName(user, newFolder);
 
-            Folder newFolder = new Folder(uniqueName, user);
             folderRepository.save(newFolder);
 
-            return new SuccessDto(HttpStatus.CREATED, "Successfully created a new folder with name: " + uniqueName);
+            return new SuccessDto(HttpStatus.CREATED,
+                    "Successfully created a new folder with name: " + newFolder.getName());
 
         } catch (Exception exception) {
             throw new FolderCreationFailureException("Failed to new create folder",
