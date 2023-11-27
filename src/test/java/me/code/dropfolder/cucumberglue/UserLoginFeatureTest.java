10a37f8ce950767fcff6639d301afb4189e163e7
jm <johan.moller040@gmail.com>
2023-11-27 11:38:20 +0100

    refactored login feature, added unsuccessful scenarios, added comments

--- src/test/java/me/code/dropfolder/cucumberglue/UserLoginFeatureTest.java
+++ src/test/java/me/code/dropfolder/cucumberglue/UserLoginFeatureTest.java
@@ -1,11 +1,14 @@
 package me.code.dropfolder.cucumberglue;
 
 import io.cucumber.java.After;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import me.code.dropfolder.controller.LoginController;
 import me.code.dropfolder.dto.SuccessDto;
 import me.code.dropfolder.dto.UserCredentialsDto;
+import me.code.dropfolder.exception.type.CouldNotFindUserException;
+import me.code.dropfolder.exception.type.InvalidCredentialsException;
 import me.code.dropfolder.service.user.UserService;
 import me.code.dropfolder.util.JpQueryUtil;
 import me.code.dropfolder.util.MockDataFactory;
@@ -14,6 +17,10 @@ import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This class contains Cucumber step definitions for testing the user login feature.
+ * It includes scenarios for successful and unsuccessful user logins.
+ */
 public class UserLoginFeatureTest {
 
     private final LoginController loginController;
@@ -22,6 +29,14 @@ public class UserLoginFeatureTest {
     private final MockDataFactory mock;
     private String mockUsername;
 
+    /**
+     * Constructor for initializing the UserLoginFeatureTest class.
+     *
+     * @param loginController The login controller used for user login operations.
+     * @param userService     The user service responsible for user-related actions.
+     * @param query           The JpQueryUtil used for querying user data.
+     * @param mock            The MockDataFactory used for creating mock data.
+     */
     public UserLoginFeatureTest(
             LoginController loginController,
             UserService userService,
@@ -33,11 +48,22 @@ public class UserLoginFeatureTest {
         this.mock = mock;
     }
 
+    /**
+     * Cucumber hook method executed after scenarios tagged with "@cleanupLoginData".
+     * Cleans up mock user data created during tests.
+     */
     @After("@cleanupLoginData")
     public void cleanUpMockData() {
         query.deleteUser(this.mockUsername);
     }
 
+    /**
+     * Cucumber step definition for the scenario where the user has a registered account.
+     * Registers a user and asserts successful registration.
+     *
+     * @param username The username for user registration.
+     * @param password The password for user registration.
+     */
     @Given("the user has a registered account with username {string} and password {string}")
     public void theUserHasARegisteredAccountWithUsernameAndPassword(String username, String password) {
         ResponseEntity<SuccessDto> registrationResult =
@@ -49,6 +75,22 @@ public class UserLoginFeatureTest {
         saveMockUsernameForDeletion(username);
     }
 
+    /**
+     * Saves the provided username for post scenario deletion.
+     *
+     * @param username The username of the user to be deleted during cleanup.
+     */
+    private void saveMockUsernameForDeletion(String username) {
+        this.mockUsername = username;
+    }
+
+    /**
+     * Cucumber step definition for the scenario where the user successfully logs in.
+     * Logs in with provided credentials and asserts successful login.
+     *
+     * @param username The username for user login.
+     * @param password The password for user login.
+     */
     @Then("the user successfully logs in with username {string} and password {string}")
     public void theUserLogsInWithUsernameAndPassword(String username, String password) {
         UserCredentialsDto mockDto = mock.createMockCredentials(username, password);
@@ -60,7 +102,60 @@ public class UserLoginFeatureTest {
         assertTrue(loginResult.getBody().getSuccess());
     }
 
-    private void saveMockUsernameForDeletion(String username) {
-        this.mockUsername = username;
+    /**
+     * Attempts a login with a valid username and an invalid password,
+     * expecting an InvalidCredentialsException to be thrown.
+     *
+     * @param validUsername   The valid username for login attempt.
+     * @param invalidPassword The invalid password for login attempt.
+     * @throws InvalidCredentialsException Thrown if the login attempt with the provided credentials is invalid.
+     */
+    @Then("the user can not log in with a valid username {string} and an invalid valid password {string}")
+    public void theUserCanNotLogInWithAValidUsernameAndAnInvalidValidPassword(String validUsername, String invalidPassword) {
+        UserCredentialsDto mockDto = mock.createMockCredentials(validUsername, invalidPassword);
+
+        assertThrows(InvalidCredentialsException.class, () -> loginController.login(mockDto));
     }
+
+    /**
+     * Attempts a login with an invalid username and a valid password,
+     * expecting an InvalidCredentialsException to be thrown.
+     *
+     * @param invalidUsername The invalid username for login attempt.
+     * @param validPassword   The valid password for login attempt.
+     * @throws InvalidCredentialsException Thrown if the login attempt with the provided credentials is invalid.
+     */
+    @And("the user can not log in with an invalid username {string} and a valid password {string}")
+    public void theUserCanNotLogInWithAnInvalidUsernameAndAValidPassword(String invalidUsername, String validPassword) {
+        UserCredentialsDto mockDto = mock.createMockCredentials(invalidUsername, validPassword);
+
+        assertThrows(InvalidCredentialsException.class, () -> loginController.login(mockDto));
+    }
+
+    /**
+     * Cucumber step definition for the scenario where the user does not have a registered account.
+     * Asserts that the user is not registered.
+     *
+     * @param username The username for checking user registration.
+     */
+    @Given("the user does not have a registered account with username {string}")
+    public void theUserDoesNotHaveARegisteredAccountWithUsername(String username) {
+        assertThrows(CouldNotFindUserException.class, () -> userService.loadUserByUsername(username));
+    }
+
+    /**
+     * Attempts a login with a non-registered user,
+     * expecting an InvalidCredentialsException to be thrown.
+     *
+     * @param username The non-registered username for login attempt.
+     * @param password The non-registered password for login attempt.
+     * @throws InvalidCredentialsException Thrown if the login attempt with the provided credentials is invalid.
+     */
+    @Then("the user can not log in with a non-registered username {string} and a non-registered password {string}")
+    public void theUserCanNotLogInWithANonRegisteredUsernameAndANonRegisteredPassword(String username, String password) {
+        UserCredentialsDto mockDto = mock.createMockCredentials(username, password);
+
+        assertThrows(InvalidCredentialsException.class, () -> loginController.login(mockDto));
+    }
+
 }
--- src/test/resources/features/2-user-login.feature
+++ src/test/resources/features/2-user-login.feature
@@ -13,3 +13,29 @@ Feature: User Login
       | username3 | Password_3 |
       | username4 | Password_4 |
       | username5 | Password_5 |
+
+  @cleanupLoginData
+  Scenario Outline: Unsuccessful registered user login
+    Given the user has a registered account with username "<username>" and password "<password>"
+    Then the user can not log in with a valid username "<username>" and an invalid valid password "<invalid_password>"
+    And the user can not log in with an invalid username "<invalid_username>" and a valid password "<password>"
+
+    Examples:
+      | username  | password   | invalid_username | invalid_password |
+      | username1 | Password_1 | usern            | Passw            |
+      | username2 | Password_2 | userna           | Passwo           |
+      | username3 | Password_3 | userna           | Passwor          |
+      | username4 | Password_4 | usernam          | Password         |
+      | username5 | Password_5 | username         | Password_        |
+
+  Scenario Outline: Unsuccessful non-registered user login
+    Given the user does not have a registered account with username "<username>"
+    Then the user can not log in with a non-registered username "<username>" and a non-registered password "<password>"
+
+    Examples:
+      | username  | password   |
+      | username1 | Password_1 |
+      | username2 | Password_2 |
+      | username3 | Password_3 |
+      | username4 | Password_4 |
+      | username5 | Password_5 |
